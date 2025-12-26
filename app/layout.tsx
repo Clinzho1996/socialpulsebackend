8367af0536ac4fe5f5428a59533d3a41c54a418b
Confidence Emonena <confidinho@yahoo.com>
2025-12-26 17:43:45 +0100

    cron jobs new fices

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-	title: "SocialPulseAI Backend",
+	title: "SocialPulseAI Backend API",
 	description: "Backend for SocialPulseAI application",
 };
 
